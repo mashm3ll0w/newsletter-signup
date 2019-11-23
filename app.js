f576317301b5d46f380de4c90bd6964068c1e255
mashm3ll0w <charlswall@live.com>
2019-11-23 17:26:20 +0300

    add server control js file

---
+++ app.js
@@ -0,0 +1,17 @@
+// jshint esversion:6
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+const request = require("request");
+const port = 1108;
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/html/signup.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
