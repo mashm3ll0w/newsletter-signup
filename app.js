af9a23646222c1699cb396c86e1429b8c0cb0cc8
mashm3ll0w <charlswall@live.com>
2019-11-23 18:34:00 +0300

    add the post route

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 const request = require("request");
 const port = 1108;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // this looks at the entire directory
+//app.use(express.static("public")); // this points the app to look into a folder called "public" that will house the css,imgs etc.
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/html/signup.html");
+});
+
+app.post("/", (req, res) => {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  res.send(`Thank you for signing up for the newsletter ${firstName} ${lastName}!! Cheers :)`);
 });
 
 app.listen(port, () => {
