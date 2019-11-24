cde39b1f215610ea5b5b4a25cb56c21863ad285b
mashm3ll0w <charlswall@live.com>
2019-11-24 17:49:01 +0300

    add port for deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const request = require("request");
-const port = 1108;
+const port = process.env.PORT || 2525;
 
 app.use(express.static(__dirname)); // this looks at the entire directory
 //app.use(express.static("public")); // this points the app to look into a folder called "public" that will house the css,imgs etc.
